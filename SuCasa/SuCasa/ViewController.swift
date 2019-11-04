319c297f7e65a013a80a20a6637aefe8320c7afb
João Victor Aquino Batista <joaov.abatista.99@gmail.com>
2019-11-04 14:25:03 -0300

    projeto criado

---
+++ SuCasa/SuCasa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuCasa
+//
+//  Created by João Victor Batista on 04/11/19.
+//  Copyright © 2019 João Victor Batista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
