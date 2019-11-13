88566f33cf47aaff6ac607268afd1bc8093d44bf
Arthur Rodrigues <arthursmr17@gmail.com>
2019-11-13 13:46:33 -0300

    your space finished

--- SuCasa/SuCasa/Controllers/SpaceViewController.swift
+++ SuCasa/SuCasa/Controllers/SpaceViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SpaceViewController: UIViewController {
 
-//  Outlet variables]
+    // Outlet variables
     @IBOutlet weak var spaceType: UITextField!
     @IBOutlet weak var propertyType: UITextField!
     
@@ -24,13 +24,56 @@ class SpaceViewController: UIViewController {
         super.viewDidLoad()
         
         setUpPickerViews()
-        setUpText()
+        setUpTextField()
+        
+        //keyboard notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    fileprivate func setUpText() {
-        spaceType.textColor = .black
-        spaceType.font = UIFont(name: "OpenSans-Regular", size: 12)
+    //Scroll when keyboard activates
+    @objc func keyboardWillShow(notification:NSNotification){
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height/3
+            }
+        }
     }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    /*The following two IBAction func is used to know what
+    text field is pressed to change the picker view */
+    
+    
+    /// This IBAction is to assign the picker view on the spaceType text field
+    /// - Parameter sender: Editing did begin
+    @IBAction func spaceTypePressed(_ sender: UITextField) {
+        spaceType.inputView = spaceTypePickerView
+        spaceTypePickerView.reloadAllComponents()
+    }
+    
+    
+    /// This IBAction is to assign the picker view on the propertyType text field
+    /// - Parameter sender: Editing did begin
+    @IBAction func propertyTypePressed(_ sender: UITextField) {
+            propertyType.inputView = spaceTypePickerView
+            spaceTypePickerView.reloadAllComponents()
+    }
+    
+    /// This method will be called when the done button at the picker view has been pressed
+    @objc func donePicker() {
+        
+        //End editing will hide the keyboard
+        self.view.endEditing(true)
+    }
+}
+
+extension SpaceViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     /// This method is for set up the picker view, create a toolbar and put a done button
     fileprivate func setUpPickerViews() {
@@ -54,42 +97,97 @@ class SpaceViewController: UIViewController {
         toolBar.setItems([flexibleSpace, doneButton], animated: false)
         
         spaceType.inputAccessoryView = toolBar
-        spaceType.inputView = spaceTypePickerView
+        propertyType.inputAccessoryView = toolBar
     }
-    
-    /// This method will be called when the done button at the picker view has been pressed
-    @objc func donePicker() {
-        
-        //End editing will hide the keyboard
-        self.view.endEditing(true)
-    }
-}
-
-extension SpaceViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return  1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return space.count
+        
+        if spaceType.isEditing {
+            return space.count
+        }
+        else if propertyType.isEditing{
+            
+            if spaceType.text == "Apartamento" {
+                return apartmentSpaceOptions.count
+                
+            } else if spaceType.text == "Casa" {
+                return houseSpaceOption.count
+            }
+        }
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return space[row]
+        
+        //This if is to know which of text fields were pressed
+        if spaceType.isEditing {
+            return space[row]
+        }
+        
+        else if propertyType.isEditing {
+            
+            /* This if is to know what option
+            selected in the spaceType */
+            if spaceType.text == "Apartamento" {
+                return apartmentSpaceOptions[row]
+                
+            } else if spaceType.text == "Casa" {
+                return houseSpaceOption[row]
+            }
+        }
+        return ""
     }
     
     /// This method get the information on the picker view and put it on the spaceType text field
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        spaceType.text = space[row]
+        
+        if spaceType.isEditing {
+            spaceType.text = space[row]
+            propertyType.isUserInteractionEnabled = true
+            //if change the option
+            clearTextField(textField: propertyType)
+        }
+        
+        else if propertyType.isEditing {
+            
+            /* Based on what the user chose in the spaceType (first picker view)
+             the propertyType options change */
+            if  spaceType.text == "Apartamento" {
+                propertyType.text = apartmentSpaceOptions[row]
+                
+            } else if spaceType.text == "Casa" {
+                propertyType.text = houseSpaceOption[row]
+            }
+        }
     }
 }
 
 extension SpaceViewController: UITextFieldDelegate {
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        spaceType.inputView = spaceTypePickerView
-        return true
+    
+    /// This method is to clear the text field
+    /// - Parameter textField: The text field that is going to be cleaned
+    func clearTextField(textField: UITextField) {
+        
+        if !textField.text!.isEmpty {
+            textField.text = ""
+        }
+    }
+    
+    fileprivate func setUpTextField() {
+        spaceType.textColor = .black
+        spaceType.font = UIFont(name: "OpenSans-Regular", size: 12)
+        
+        /* Set user interaction to false because it's
+           necessary to choose an option in the first
+           picker view then it will be available */
+        propertyType.isUserInteractionEnabled = false
+        propertyType.textColor = .black
+        propertyType.font = UIFont(name: "OpenSans-Regular", size: 12)
     }
     
 }
