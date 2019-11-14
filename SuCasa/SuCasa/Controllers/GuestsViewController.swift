173bf10a45e1c2d190fe1314da98e5a29c78fe91
João Victor Aquino Batista <joaov.abatista.99@gmail.com>
2019-11-14 13:18:24 -0300

    info from SpaceViewController p/ GuestsViewController

--- SuCasa/SuCasa/Controllers/GuestsViewController.swift
+++ SuCasa/SuCasa/Controllers/GuestsViewController.swift
@@ -14,7 +14,12 @@ class GuestsViewController: UIViewController {
     @IBOutlet weak var bedroomNumberTextField: UITextField!
     @IBOutlet weak var bedNumberTextField: UITextField!
     
+    var propertyCreated: Property!
+    
     override func viewDidLoad() {
+        
+        print(propertyCreated.space)
+        print(propertyCreated.type)
         super.viewDidLoad()
         
         setUpTextFields()
