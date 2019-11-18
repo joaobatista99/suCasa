2942d9d10bf443187c9bf65501e68e4d5288ff0a
João Victor Aquino Batista <joaov.abatista.99@gmail.com>
2019-11-18 14:04:43 -0300

    lcoationViewController created

---
+++ SuCasa/SuCasa/Controllers/LocationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LocationViewController.swift
+//  SuCasa
+//
+//  Created by João Victor Batista on 18/11/19.
+//  Copyright © 2019 João Victor Batista. All rights reserved.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+    
+    var property: Property!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
