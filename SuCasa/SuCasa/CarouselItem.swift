a0cbad964823ed3ffafad44badee669957a97e6a
João Victor Aquino Batista <joaov.abatista.99@gmail.com>
2019-11-21 16:02:32 -0300

    property detail carousel

---
+++ SuCasa/SuCasa/CarouselItem.swift
@@ -0,0 +1,40 @@
+//
+//  CarouselItem.swift
+//  SuCasa
+//
+//  Created by João Victor Batista on 21/11/19.
+//  Copyright © 2019 João Victor Batista. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class CarouselItem : UIView {
+    static let carouselItemNib = "CarouselItem"
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override init(frame:CGRect){
+        super.init(frame:frame)
+        initWithNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initWithNib()
+    }
+    
+    convenience init(image: UIImage){
+        self.init()
+        self.imageView.image = image
+    }
+    
+    fileprivate func initWithNib() {
+        Bundle.main.loadNibNamed(CarouselItem.carouselItemNib, owner: self, options: nil)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        addSubview(contentView)
+    }
+}
