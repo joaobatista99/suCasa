02f875c3485d03b1732fbb939703a7aa8e654cfa
João Victor Aquino Batista <joaov.abatista.99@gmail.com>
2020-02-03 10:31:40 -0300

    Ong Collection Dyanimc Text for iPhone 11

--- SuCasa/SuCasa/Controllers/OngsCollectionViewController.swift
+++ SuCasa/SuCasa/Controllers/OngsCollectionViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 import SDWebImage
 
 class OngsCollectionViewController: UIViewController {
-
-
+    
+    
     @IBOutlet weak var OngsCollectionView: UICollectionView!
     
     var ongs: [Ong] = []
@@ -24,27 +24,32 @@ class OngsCollectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         OngsCollectionView.delegate = self
         OngsCollectionView.dataSource = self
         setLocalizedStrings()
         
         self.navigationController?.navigationBar.tintColor = .white
         
+        
     }
     
-
     override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           tabBarController?.tabBar.isHidden = true
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-           super.viewWillDisappear(animated)
-           tabBarController?.tabBar.isHidden = false
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+        
+        if ongsCollectionTitleLabel.font.pointSize > 52.0 {
+            ongsCollectionTitleLabel.font = ongsCollectionTitleLabel.font.withSize(52.0)
+            ongsCollectionDescriptionLabel.font = ongsCollectionDescriptionLabel.font.withSize(40.0)
+        }
     }
     
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier  == "showOngDetail" {
             let detailOngVc = segue.destination as? OngDetailViewController
@@ -56,7 +61,7 @@ class OngsCollectionViewController: UIViewController {
         ongsCollectionTitleLabel.text = NSLocalizedString("Conheça nossas ONGs parceiras", comment: "Conheça nossas ONGs parceiras - Label")
         ongsCollectionDescriptionLabel.text = NSLocalizedString("Entre em contato com uma ONG e faça o agendamento para conhecer uma locação e o locador.", comment: "Entre em contato com uma ONG e faça o agendamento para conhecer uma locação e o locador. - Label")
     }
-
+    
     
 }
 
@@ -65,10 +70,10 @@ class OngsCollectionViewController: UIViewController {
 extension OngsCollectionViewController :  UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     
     //setting number of  items in collectio view
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return self.ongs.count
-          
-        }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.ongs.count
+        
+    }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
@@ -76,46 +81,64 @@ extension OngsCollectionViewController :  UICollectionViewDataSource, UICollecti
         
         self.performSegue(withIdentifier: "showOngDetail", sender: self)
     }
-        
-      
-      //setting up collection view cell
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            
-            let cell = OngsCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! OngCell
-            
-            let ong = ongs[indexPath.row]
-            
-            let imageUrl = URL(string: ong.url[0])
-            
-            cell.imageView.sd_setImage(with: imageUrl,
-                                       placeholderImage: placeHolderImage,
-                                       options: SDWebImageOptions.lowPriority,
-                                       context: nil,
-                                       progress: nil) { (downloadedImage, error, cacheType, downloadURL) in
-                                        if let error = error {
-                                            print("Error downloading the image: \(error.localizedDescription)")
-                                        } else {
-                                            print("Successfully downloaded image: \(String(describing: downloadURL?.absoluteString))")
-                                        }
-            }
-            
-            cell.ongName.text = ong.name
-            
-            return  cell
-        }
     
-
-      //setting line space between cells
-      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-              
-          return 13
-      }
-          
-      
-      //setting space between cells
-      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-              
-          return 10
-      }
+    
+    //setting up collection view cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = OngsCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! OngCell
+        
+        let ong = ongs[indexPath.row]
+        
+        let imageUrl = URL(string: ong.url[0])
+        
+        cell.imageView.sd_setImage(with: imageUrl,
+                                   placeholderImage: placeHolderImage,
+                                   options: SDWebImageOptions.lowPriority,
+                                   context: nil,
+                                   progress: nil) { (downloadedImage, error, cacheType, downloadURL) in
+                                    if let error = error {
+                                        print("Error downloading the image: \(error.localizedDescription)")
+                                    } else {
+                                        print("Successfully downloaded image: \(String(describing: downloadURL?.absoluteString))")
+                                    }
+        }
+        
+        cell.ongName.text = ong.name
+        
+        if ongsCollectionTitleLabel.font.pointSize >= 52.0 {
+            cell.ongName.font = cell.ongName.font.withSize(40.0)
+        }
+        
+        return  cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var size = CGSize(width: 170.0, height: 140.0)
+        
+        if ongsCollectionTitleLabel.font.pointSize >= 44.0 {
+            size = CGSize(width: 230.0, height: 200.0)
+        }
+        
+        
+        return size
+        
+    }
+    
+    
+    
+    //setting line space between cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 13
+    }
+    
+    
+    //setting space between cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 10
+    }
     
 }
